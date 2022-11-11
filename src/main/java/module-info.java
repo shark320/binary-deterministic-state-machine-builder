6b74ce5b72f0be3bae8d88c316e2cde99b8c7b1c
Volodymyr Pavlov <shark32@students.zcu.cz>
2022-11-11 15:34:29 +0100

    module-info update.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,6 @@ module org.example {
 
     opens com.vpavlov to javafx.fxml;
     exports com.vpavlov;
-    exports com.vpavlov.controller;
-    opens com.vpavlov.controller to javafx.fxml;
+    exports com.vpavlov.visualization.controller;
+    opens com.vpavlov.visualization.controller to javafx.fxml;
 }
