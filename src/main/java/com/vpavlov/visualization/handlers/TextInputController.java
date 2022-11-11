a376d71def5dd343184d58dad148ebc95035143d
Volodymyr Pavlov <shark32@students.zcu.cz>
2022-11-11 15:31:23 +0100

    Text input controller for the text field.

---
+++ src/main/java/com/vpavlov/visualization/handlers/TextInputController.java
@@ -0,0 +1,71 @@
+package com.vpavlov.visualization.handlers;
+
+import com.vpavlov.App;
+import com.vpavlov.machine.Alphabet;
+import com.vpavlov.machine.Machine;
+import com.vpavlov.visualization.controller.PrimaryController;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.TextField;
+
+public class TextInputController implements ChangeListener<String> {
+
+    private final Alphabet alphabet;
+
+    private final TextField input;
+
+    private final Machine machine;
+
+    private PrimaryController controller;
+
+    public TextInputController(PrimaryController controller) {
+        //bind machine-related objects
+        this.alphabet = App.getAlphabet();
+        this.machine = App.getMachine();
+        this.input = controller.input;
+    }
+
+    private boolean isLoop = false;
+
+
+    @Override
+    public void changed(ObservableValue<? extends String> observableValue, String s, String s1) {
+        String newSymbol;
+        if (s == null || s1 == null || isLoop)
+            return;
+        //App.showInfoAlert(String.format("s: %s, s1: %s", s, s1));
+        //App.showInfoAlert(alphabet.getSymbols().toString());
+        isLoop = true;
+        //TODO propagate event
+        //prevent deleting
+        if (s1.length() < s.length()) {
+            input.setText(s);
+        }else {
+            newSymbol = s1.substring(s.length());
+            //App.showInfoAlert(String.format("s: %s, s1: %s, new: %s", s, s1, newSymbol));
+            if (checkSymbol(newSymbol)) {
+                input.setText(s1);
+                makeTransition(newSymbol);
+            } else {
+                input.setText(s);
+            }
+        }
+        isLoop = false;
+    }
+
+    private boolean checkSymbol(String s){
+        for (String symbol : alphabet.getSymbols()){
+            if (s.equals(symbol)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void makeTransition(String symbol){
+        String prevState = machine.getCurrentState();
+        String newState = machine.transition(symbol);
+        App.showInfoAlert(String.format("[%s] --%s--> [%s]",prevState,symbol, newState));
+    }
+}
