536ec6b528d878c5952e162938c83a9ac7baeda9
Volodymyr Pavlov <shark32@students.zcu.cz>
2022-11-15 21:36:42 +0100

    Add exports.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,8 @@ module org.example {
     exports com.vpavlov;
     exports com.vpavlov.visualization.controller;
     opens com.vpavlov.visualization.controller to javafx.fxml;
+    exports com.vpavlov.visualization.machineBuilder.controller;
+    opens com.vpavlov.visualization.machineBuilder.controller to javafx.fxml;
+    exports com.vpavlov.visualization.selection_window.controller;
+    opens com.vpavlov.visualization.selection_window.controller to javafx.fxml;
 }
