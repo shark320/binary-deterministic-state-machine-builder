b9c8b40c753538103008a2a5a042917bd37cd9e7
Volodymyr Pavlov <shark32@students.zcu.cz>
2022-11-15 21:39:36 +0100

    Make initializable.

--- src/main/java/com/vpavlov/visualization/controller/PrimaryController.java
+++ src/main/java/com/vpavlov/visualization/controller/PrimaryController.java
@@ -1,34 +1,49 @@
 package com.vpavlov.visualization.controller;
 
-import com.vpavlov.App;
-import com.vpavlov.visualization.handlers.MachineNodeMouseClickHandler;
-import com.vpavlov.visualization.handlers.MouseClickHandler;
+import com.vpavlov.proprety.AppProperties;
+import com.vpavlov.visualization.draw_model.MachineNode;
+import com.vpavlov.visualization.machineBuilder.handlers.KeyPressedHandler;
 import com.vpavlov.visualization.handlers.TextInputController;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.Group;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
-public class PrimaryController   {
+import java.net.URL;
+import java.util.*;
+
+public class PrimaryController implements Initializable {
+
+    public static final double MINIMAL_NODE_DISTANCE = 20;
+
+    private static final AppProperties properties = AppProperties.getInstance();
 
     @FXML
-    public Pane canvas;
+    private Pane canvas;
 
     @FXML
     public TextField input;
 
-    public Group machineNodes = new Group();
+    private Map<String,MachineNode> machineNodes = new HashMap();
 
-    public static MachineNodeMouseClickHandler machineNodeMouseClickHandler = new MachineNodeMouseClickHandler();
 
-    public void init(){
-        canvas.getChildren().add(machineNodes);
-        canvas.setOnMouseClicked(new MouseClickHandler(this));
+    public void init() {
+        canvas.getChildren().addAll(machineNodes.values());
         input.textProperty().addListener(new TextInputController(this));
 
     }
 
+    public Pane getCanvas(){
+        return canvas;
+    }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        double paneWidth = Double.parseDouble(properties.getProperty("canvas-width"));
+        double paneHeight = Double.parseDouble(properties.getProperty("canvas-height"));
+        getCanvas().setPrefWidth(paneWidth);
+        getCanvas().setPrefHeight(paneHeight);
+        getCanvas().setMinWidth(paneWidth);
+        getCanvas().setMinHeight(paneHeight);
+    }
 }
