ace0343ba8e27958b0ddd6ba1782500aea745601
Volodymyr Pavlov <shark32@students.zcu.cz>
2022-11-17 19:40:24 +0100

    initial 2.

--- src/main/java/com/vpavlov/visualization/selection_window/SelectionWindow.java
+++ src/main/java/com/vpavlov/visualization/selection_window/SelectionWindow.java
@@ -10,12 +10,17 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SelectionWindow extends Stage {
 
     private static final AppProperties properties = AppProperties.getInstance();
 
     private static final String TITLE = properties.getProperty("selection-title");
+
+    private boolean closeRequest = false;
 
     private final SelectionWindowController controller;
 
@@ -24,25 +29,48 @@ public class SelectionWindow extends Stage {
         FXMLLoader loader = new FXMLLoader(App.class.getResource("selectionWindow.fxml"));
         Scene scene = new Scene(loader.load());
         controller = loader.getController();
-        scene.getStylesheets().add(App.class.getResource(properties.getProperty("custom-stylesheet")).toExternalForm());
+        scene.getStylesheets().add(App.class.getResource("selection-window.css").toExternalForm());
         this.initModality(Modality.APPLICATION_MODAL);
         this.setTitle(TITLE);
         this.setResizable(false);
         this.setScene(scene);
         this.initModality(Modality.APPLICATION_MODAL);
         this.sizeToScene();
+        this.setOnCloseRequest(e-> closeRequest = true);
     }
 
-    public static String showSelectionAndWait(){
+    public static String showRadioSelectionAndWait(Collection<String> variants) {
+        SelectionWindow instance = null;
         try {
-            SelectionWindow instance = new SelectionWindow();
-            instance.showAndWait();
-            return instance.controller.getSelectedButton();
-
+            instance = new SelectionWindow();
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-        return null;
+        String selected;
+        instance.controller.setRadioVariants(variants);
+        instance.showAndWait();
+        selected = instance.controller.getSelectedRadioButton();
+        instance.controller.poor();
+        return selected;
+    }
+
+    public static Set<String> showCheckSelectionAndWait(Collection<String> variants) {
+        SelectionWindow instance = null;
+        try {
+            instance = new SelectionWindow();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Set<String> checkedVariants;
+        instance.controller.setCheckVariants(variants);
+        instance.showAndWait();
+        if (!instance.closeRequest) {
+            checkedVariants = instance.controller.getCheckedVariants();
+        }else{
+            checkedVariants = null;
+        }
+        instance.controller.poor();
+        return checkedVariants;
     }
 
 }
--- src/main/java/com/vpavlov/visualization/selection_window/controller/SelectionWindowController.java
+++ src/main/java/com/vpavlov/visualization/selection_window/controller/SelectionWindowController.java
@@ -1,17 +1,24 @@
 package com.vpavlov.visualization.selection_window.controller;
 
 import com.vpavlov.proprety.AppProperties;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 public class SelectionWindowController implements Initializable {
 
@@ -25,28 +32,75 @@ public class SelectionWindowController implements Initializable {
 
     private ToggleGroup toggleGroup;
 
+    private Set<CheckBox> checkBoxes;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        selectButton.setOnAction(e->{
-            RadioButton selected = (RadioButton)toggleGroup.getSelectedToggle();
-            if (selected != null){
+
+        selectButton.setOnAction(new EventHandler<>() {
+
+            private void closeStage(ActionEvent e){
                 Node source = (Node) e.getSource();
                 Stage stage = (Stage) source.getScene().getWindow();
                 stage.close();
             }
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if (toggleGroup != null) {
+                    if (getSelectedRadioButton() != null) {
+                        closeStage(actionEvent);
+                    }
+                }else{
+                    if (checkBoxes != null){
+                        if (getCheckedVariants() != null){
+                            closeStage(actionEvent);
+                        }
+                    }
+                }
+            }
         });
-        String[] symbols = properties.getProperty("alphabet").split(",");
+    }
+
+    public void setRadioVariants(Collection<String> variants){
         toggleGroup = new ToggleGroup();
-        for(String symbol : symbols){
-            RadioButton rb = new RadioButton(symbol);
+        for(String variant : variants){
+            RadioButton rb = new RadioButton(variant);
             rb.setToggleGroup(toggleGroup);
             vBox.getChildren().add(rb);
         }
     }
 
-    public String getSelectedButton(){
+    public String getSelectedRadioButton(){
         RadioButton selected = (RadioButton)toggleGroup.getSelectedToggle();
         return selected == null ? null : selected.getText();
+    }
+
+    public void setCheckVariants(Collection<String> variants){
+        checkBoxes = new HashSet<>();
+        for(String variant : variants){
+            CheckBox checkBox = new CheckBox(variant);
+            checkBox.setTextFill(Color.AQUA);
+            vBox.getChildren().add(checkBox);
+            checkBoxes.add(checkBox);
+        }
+    }
+
+    public Set<String> getCheckedVariants(){
+        Set<String> checkedVariants = new HashSet<>();
+        for (CheckBox checkBox: checkBoxes){
+            if (checkBox.isSelected()){
+                checkedVariants.add(checkBox.getText());
+
+            }
+        }
+        return checkedVariants.isEmpty()?null : checkedVariants;
+    }
+
+
+    public void poor(){
+        vBox.getChildren().clear();
+        checkBoxes = null;
+        toggleGroup = null;
     }
 }
