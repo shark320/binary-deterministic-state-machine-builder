aff6c7968e5300ee8bf9d7f763333f04b7582ee3
Volodymyr Pavlov <shark32@students.zcu.cz>
2022-11-17 19:41:58 +0100

    Actions with titles.

--- src/main/java/com/vpavlov/visualization/draw_model/TransitionLine.java
+++ src/main/java/com/vpavlov/visualization/draw_model/TransitionLine.java
@@ -2,6 +2,7 @@ package com.vpavlov.visualization.draw_model;
 
 import javafx.scene.layout.Region;
 
+import java.util.Collection;
 import java.util.Set;
 
 public class TransitionLine extends Region{
@@ -12,22 +13,17 @@ public class TransitionLine extends Region{
 
     private final ATitledLine line;
 
-    public TransitionLine(MachineNode start, MachineNode end, String title) {
+    public TransitionLine(MachineNode start, MachineNode end, Collection<String> titles) {
         this.start = start;
         this.end = end;
         if (end != start) {
-            line = new NodeArrow(start, end, title);
+            line = new NodeArrow(start, end, titles);
         }else{
-            line = new LoopLine(start, title);
+            line = new LoopLine(start, titles);
         }
         this.getChildren().add(line);
     }
 
-    public TransitionLine(MachineNode node, String title){
-        this.start = this.end = node;
-        line = new LoopLine(node, title);
-        this.getChildren().add(line);
-    }
 
     public MachineNode getStart(){
         return this.start;
@@ -38,10 +34,35 @@ public class TransitionLine extends Region{
     }
 
     public void addTitle(String title){
+        line.addTitle(title);
+    }
 
+    public void removeTitle(String title){
+        line.removeTitle(title);
+    }
+
+    public boolean isEmptyTitles(){
+        return line.getTitles().isEmpty();
     }
 
     public Set<String> getTitles(){
         return line.getTitles();
+    }
+
+    public void addTitles(Collection<String> titles) {
+        line.addTitles(titles);
+    }
+
+    public void removeTitles(Collection<String> symbols) {
+        line.removeTitles(symbols);
+    }
+
+    public boolean containsTitles(String title){
+       return line.titles.contains(title);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("TransitionLine <%s> --%s--> <%s>", start.getTitle(), line.getTitles(), end.getTitle());
     }
 }
