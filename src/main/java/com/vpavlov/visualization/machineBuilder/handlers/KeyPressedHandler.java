f47fb3473fb0e9871d4c111436afa1df12f35633
Volodymyr Pavlov <shark32@students.zcu.cz>
2022-11-17 19:31:05 +0100

    Add events for keys.

--- src/main/java/com/vpavlov/visualization/machineBuilder/handlers/KeyPressedHandler.java
+++ src/main/java/com/vpavlov/visualization/machineBuilder/handlers/KeyPressedHandler.java
@@ -2,6 +2,7 @@ package com.vpavlov.visualization.machineBuilder.handlers;
 
 import com.vpavlov.visualization.machineBuilder.controller.MachineBuilderController;
 import javafx.event.EventHandler;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 public class KeyPressedHandler implements EventHandler<KeyEvent> {
@@ -14,11 +15,21 @@ public class KeyPressedHandler implements EventHandler<KeyEvent> {
 
     @Override
     public void handle(KeyEvent keyEvent) {
-        if (keyEvent.isControlDown()){
-            switch (keyEvent.getCode()){
-                case C ->{
-                    controller.createTransition();
-                }
+        if (keyEvent.isControlDown()) {
+            switch (keyEvent.getCode()) {
+                case C -> controller.createTransition();
+
+                case R -> controller.removeTransition();
+
+                case S -> controller.startNodeSetting();
+
+                case D -> controller.removeNode();
+
+                case F -> controller.finalNodeSetting();
+            }
+        } else {
+            if (keyEvent.getCode() == KeyCode.ESCAPE) {
+                controller.unselectAll();
             }
         }
     }
