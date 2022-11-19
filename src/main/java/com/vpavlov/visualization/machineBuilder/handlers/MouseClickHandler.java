5feba8e076fdb7f047498d6bcb39a2aaea75c96d
Volodymyr Pavlov <shark32@students.zcu.cz>
2022-11-19 17:05:01 +0100

    getters change.

--- src/main/java/com/vpavlov/visualization/machineBuilder/handlers/MouseClickHandler.java
+++ src/main/java/com/vpavlov/visualization/machineBuilder/handlers/MouseClickHandler.java
@@ -1,11 +1,6 @@
 package com.vpavlov.visualization.machineBuilder.handlers;
 
-import com.vpavlov.visualization.controller.PrimaryController;
-import com.vpavlov.visualization.draw_model.Arrow;
-import com.vpavlov.visualization.draw_model.LoopLine;
 import com.vpavlov.visualization.draw_model.MachineNode;
-import com.vpavlov.visualization.draw_model.TransitionLine;
-import com.vpavlov.visualization.machineBuilder.MachineBuilderStage;
 import com.vpavlov.visualization.machineBuilder.controller.MachineBuilderController;
 import javafx.event.EventHandler;
 import javafx.geometry.Point2D;
@@ -20,7 +15,7 @@ public class MouseClickHandler implements EventHandler<MouseEvent> {
 
     @Override
     public void handle(MouseEvent mouseEvent) {
-        controller.getCanvas().requestFocus();
+        controller.getCanvasPane().requestFocus();
         if (!controller.isDrag) {
             double x = mouseEvent.getX();
             double y = mouseEvent.getY();
